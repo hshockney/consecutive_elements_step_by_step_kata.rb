22d6d65f87e0449abc5ef4dcbc6fe26a3954e930
Heather Sockney <shockney@atlanticbb.net>
2015-09-15 16:07:05 -0400

    made changes to code to allow for tests to pass

--- consecutive_elements_step_by_step_kata.rb
+++ consecutive_elements_step_by_step_kata.rb
@@ -7,41 +7,18 @@ def find_max_consecutive_identical_elements(any_array)
     consecutive_elements << value 
 	elsif 
 	value == consecutive_elements[-1]
-  consecutive_elements << value
+    consecutive_elements << value
   
-   elsif 
-   any_array[-2]== any_array[-1]
-   consecutive_elements=[]
-   consecutive_elements<< value
-  
-
- 
- else
+  else
     count_of_consecutive_elements << consecutive_elements.count
-consecutive_elements = []
+    consecutive_elements = []
+    consecutive_elements<< value
 end
 end
   count_of_consecutive_elements << consecutive_elements.count
-  count_of_consecutive_elements.max 
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ count_of_consecutive_elements.max 
+ 
+ 
 
 #if any_array.length==0
 #0
